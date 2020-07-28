f631a126d1f6ee196f1e3317f40c9dba5305ad78
cinyee1214 <xinyizhao1214@gmail.com>
2020-07-28 19:26:48 -0400

    Build Control component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
@@ -38,8 +38,8 @@ class BurgerIngredient extends Component {
   
 }
 
-BurgerIngredient.PropTypes = {
-  type: PropTypes.string.isRequired
+BurgerIngredient.propTypes = {
+  type: propTypes.string.isRequired
 };
 
 export default BurgerIngredient;
