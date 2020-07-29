150761b8cf07622e4fe58bcf82df28519d1b4f44
cinyee1214 <xinyizhao1214@gmail.com>
2020-07-29 10:35:05 -0400

    Order Button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,19 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  }
+
+  updatePurchaseState (ingredients) {
+    const sum = Object.keys(ingredients)
+                .map(igKey => {
+                  return ingredients[igKey];
+                })
+                .reduce((sum, el) => {
+                  return sum + el;
+                }, 0);
+    this.setState({purchasable: sum > 0});
   }
 
   addIngredientHandler = (type) => {
@@ -36,6 +48,7 @@ class BurgerBuilder extends Component {
 
     this.setState({totalPrice: newPrice,
                   ingredients: updatedIngredients});
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -54,6 +67,8 @@ class BurgerBuilder extends Component {
 
     this.setState({totalPrice: newPrice,
                   ingredients: updatedIngredients}); 
+    
+    this.updatePurchaseState(updatedIngredients);
   }
 
   render() {
@@ -71,6 +86,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          purchasable={this.state.purchasable}
           price={this.state.totalPrice} />
       </Aux>
     );
