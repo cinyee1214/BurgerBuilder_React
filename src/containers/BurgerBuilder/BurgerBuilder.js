8f8d64138db89158cd72402a220167fae7d835c2
cinyee1214 <xinyizhao1214@gmail.com>
2020-07-29 13:51:09 -0400

    Add Price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -101,6 +101,7 @@ class BurgerBuilder extends Component {
                 modalClosed={this.purchaseCancelHandler}>
           <OrderSummary 
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
             purchaseCancelled={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler} />
         </Modal>
