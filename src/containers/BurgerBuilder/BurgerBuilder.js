f3d1049d005dad5ae0ebcd9a1baa4cd57fa59b91
cinyee1214 <xinyizhao1214@gmail.com>
2020-08-01 20:32:20 -0400

    sending post request to server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -83,7 +84,25 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    // alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      prices: this.state.totalPrice,
+      customer: {
+        name: 'Max S',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: '07306',
+          country: 'US'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+
+    axios.post('/orders.json', order)
+          .then(response => console.log(response))
+          .catch(error => console.log(error));
   }
   
 
