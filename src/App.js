641647f9aa118de68c57f6b6e392d1baadbfdecc
cinyee1214 <xinyizhao1214@gmail.com>
2020-08-01 21:58:58 -0400

    remove old interceptors

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 
 class App extends Component {
+  state = {
+    show: true
+  };
+
+  componentWillMount () {
+    setTimeout(() => {
+      this.setState({show: false});
+    }, 5000);
+  }
+
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          {this.state.show ? <BurgerBuilder /> : null}
         </Layout>
       </div>
     );
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -6,16 +6,22 @@ const withErrorHandler = (WrappedComponent, axios) => {
   return class extends Component {
     state = {
       error: null
-    }
+    };
 
     componentWillMount () {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
           this.setState({error: null});
           return req;
       });
-      axios.interceptors.response.use(res => res, error => {
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
           this.setState({error: error});
       });
+    }
+
+    componentWillUnmount () {
+      console.log('Will Unmount', this.reqInterceptor, this.resInterceptor);
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
